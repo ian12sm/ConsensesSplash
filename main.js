b3e40a984398ea643655b1fa842d57bd2f288e3d
Ian Noyes <ian@12southmusic.com>
2014-04-16 16:35:55 -0500

    try again

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $.backstretch('img/big-elephant.jpg');
 //Parallax?
 function parallax(){
     var scrolled = $(window).scrollTop();
-    $('.bg-white').css('top', -(scrolled) + 475 + 'px');
+    $('.bg-white').css('top', -(scrolled) + 550 + 'px');
 }
 //Call paralax on scroll
 $(window).scroll(function(e){
